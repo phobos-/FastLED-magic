ec167345275059c5a5e515d1a688e6eb07213d84
phobos- <pawel.m.stefanski@gmail.com>
2018-02-28 10:49:15 +0100

    Added plasma and matrix_ray

---
+++ plasma/plasma.ino
@@ -0,0 +1,61 @@
+#include "FastLED.h"                                          // FastLED library. Preferably the latest copy of FastLED 2.1.
+
+#define qsubd(x, b)  ((x>b)?b:0)                              // Digital unsigned subtraction macro. if result <0, then => 0. Otherwise, take on fixed value.
+#define qsuba(x, b)  ((x>b)?x-b:0)                            // Analog Unsigned subtraction macro. if result <0, then => 0
+
+// Fixed definitions cannot change on the fly.
+#define LED_DT 0                                              // Data pin to connect to the strip.
+#define LED_CK 1                                              // Clock pin for WS2801 or APA102.
+#define COLOR_ORDER BGR                                       // It's GRB for WS2812 and BGR for APA102.
+#define LED_TYPE APA102                                       // Using APA102, WS2812, WS2801. Don't forget to change LEDS.addLeds.
+#define NUM_LEDS 28                                           // Number of LED's.
+
+uint8_t max_bright = 64;                                      // Overall brightness definition. It can be changed on the fly.
+struct CRGB leds[NUM_LEDS];                                   // Initialize our LED array.
+
+CRGBPalette16 currentPalette;                                 // Palette definitions
+CRGBPalette16 targetPalette;
+TBlendType currentBlending = LINEARBLEND;
+
+void setup() {
+
+  LEDS.addLeds<LED_TYPE, LED_DT, LED_CK, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(max_bright);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 500);             // FastLED Power management set at 5V, 500mA.
+  currentPalette = OceanColors_p;
+
+}
+
+void loop () {
+
+  EVERY_N_MILLISECONDS(50) {                                  // FastLED based non-blocking delay to update/display the sequence.
+    plasma();
+  }
+
+  EVERY_N_MILLISECONDS(100) {
+    uint8_t maxChanges = 24; 
+    nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);   // AWESOME palette blending capability.
+  }
+
+  EVERY_N_MILLISECONDS(5*1000) {                       // Change the target palette to a random one every 5 seconds.
+    uint8_t baseC = random8();                         // You can use this as a baseline colour if you want similar hues in the next line.
+    targetPalette = CRGBPalette16(CHSV(baseC+random8(32), 192, random8(128,255)), CHSV(baseC+random8(32), 255, random8(128,255)), CHSV(baseC+random8(32), 192, random8(128,255)), CHSV(baseC+random8(32), 255, random8(128,255)));
+  }
+
+  FastLED.show();
+
+}
+
+void plasma() {                                                 // This is the heart of this program. Sure is short. . . and fast.
+
+  int thisPhase = beatsin8(6,-64,64);                           // Setting phase change for a couple of waves.
+  int thatPhase = beatsin8(7,-64,64);
+
+  for (int k=0; k<NUM_LEDS; k++) {                              // For each of the LED's in the strand, set a brightness based on a wave as follows:
+
+    int colorIndex = cubicwave8((k*23)+thisPhase)/2 + cos8((k*15)+thatPhase)/2;           // Create a wave and add a phase change and add another wave with its own phase change.. Hey, you can even change the frequencies if you wish.
+    int thisBright = qsuba(colorIndex, beatsin8(7,0,96));                                 // qsub gives it a bit of 'black' dead space by setting sets a minimum value. If colorIndex < current value of beatsin8(), then bright = 0. Otherwise, bright = colorIndex..
+
+    leds[k] = ColorFromPalette(currentPalette, colorIndex, thisBright, currentBlending);  // Let's now add the foreground colour.
+  }
+}
